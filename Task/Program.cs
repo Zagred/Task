71a6c421c4c99c81f09b8c0efe365488c8a59ae8
Plamen pandev <looopooo45@gmail.com>
2024-02-22 12:14:41 +0200

    .

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,20 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using HtmlAgilityPack;
+using Microsoft.Office.Interop.Excel;
+using TestLibrary;
+
 namespace Task
 {
     internal class Program
     {
+
         private static List<string> Init_Dict(string folder)
         {
             List<string> folderContent = new List<string>();
@@ -42,9 +48,12 @@ namespace Task
 
         private static void xmlArguments(string path)
         {
+            
             List<string> Folders = Init_Dict(path);
+            
             List<string> xmlFiles = XmlFiles(Folders);
             List<string> arguments = new List<string>();
+
             var htmlDoc = new HtmlDocument();
 
             foreach (var xml in xmlFiles)
@@ -55,8 +64,8 @@ namespace Task
                 foreach (var tag in tags)
                 {
                     string formatElement = tag.InnerText.Trim();
+                    
                     arguments.Add(formatElement);
-
                 }
             }
             CallMethodByName(arguments);
@@ -65,6 +74,7 @@ namespace Task
 
         public static void CallMethodByName(List<string> args)
         {
+
             List<string> notMonitoring = new List<string>();
             foreach (var arg in args)
             {
@@ -79,8 +89,9 @@ namespace Task
                         if (Method != null)
                         {
                             Method.CustomAttributes.First().ToString();
-
+                            Console.WriteLine(Method.CustomAttributes.First().ToString());
                         }
+                        
                     }
                     catch
                     {
@@ -94,12 +105,50 @@ namespace Task
             }
         }
 
+
+        public static void LibraryTest()
+        {
+            Assembly SampleAssembly, LibraryAssembly;
+            SampleAssembly = Assembly.LoadFrom(@"C:\Users\PlamenPandev\Desktop\Projects\TaskSecond\TaskSecond\bin\Release\TaskSecond.exe");
+            //LibraryAssembly = Assembly.LoadFrom(@"C:\Users\PlamenPandev\Desktop\Projects\TestLibrary\TestLibrary\bin\Debug\TestLibrary.dll");
+            LibraryAssembly = Assembly.LoadFrom(@"C:\Users\PlamenPandev\Desktop\Projects\TestLibrary\TestLibrary\bin\Debug\TestLibrary.dll");
+            List<string> arguments = new List<string>();
+            List<string> libArguments = new List<string>();
+            // MethodInfo MethodInfo = SampleAssembly.GetType("TaskSecond.Program") 
+            //MethodInfo MethodInfo = SampleAssembly.GetType("TestLibrary.Library")
+            // MethodInfo[] Method = SampleAssembly.GetType("TestLibrary.Library").GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            var libMethods = LibraryAssembly.GetType("TestLibrary.Library").GetMethods();
+            var mainMethods=SampleAssembly.GetType("TaskSecond.Program").GetMethods();
+            
+            foreach(var method in libMethods)
+            {
+                libArguments.Add(method.Name);
+                foreach (var arg in mainMethods)
+                {
+                    if (method.Name == arg.Name)
+                    {
+                        arguments.Add(arg.Name);
+                    }
+                }
+            }
+            
+            foreach(var method in arguments)
+            {
+                libArguments.Remove(method);
+            }
+            foreach (var arg in libArguments)
+            {
+                Console.WriteLine(arg);
+            }
+
+        }
         static void Main(string[] args)
         {
 
-            string basePath = @"C:\Users\PlamenPandev\Desktop\Projects\Task\Task";
-            xmlArguments(basePath);
-
+            //string basePath = @"C:\Users\PlamenPandev\Desktop\Projects\shared";
+            //xmlArguments(basePath);
+            LibraryTest();
+            
         }
     }
 }
